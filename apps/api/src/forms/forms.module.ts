eac4f886aa08ac4e62e8d5bb6e53a6d5ba2b88c8
murugu21 <murugu2001@gmail.com>
2026-06-16 14:33:29 +0530

    refactor(api): extract FormRepository port + Drizzle adapter

--- apps/api/src/forms/forms.module.ts
+++ apps/api/src/forms/forms.module.ts
@@ -1,9 +1,14 @@
 import { Module } from "@nestjs/common";
 import { FormsController } from "./forms.controller";
 import { FormsService } from "./forms.service";
+import { FORM_REPOSITORY } from "./forms.repository";
+import { DrizzleFormRepository } from "./forms.repository.drizzle";
 
 @Module({
   controllers: [FormsController],
-  providers: [FormsService],
+  providers: [
+    FormsService,
+    { provide: FORM_REPOSITORY, useClass: DrizzleFormRepository },
+  ],
 })
 export class FormsModule {}
