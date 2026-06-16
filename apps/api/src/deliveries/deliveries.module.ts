0dcb34ca5f3c6a5b0e5ef476e73a21ee679b3e34
murugu21 <murugu2001@gmail.com>
2026-06-16 14:30:54 +0530

    refactor(api): extract DeliveryRepository port + Drizzle adapter

--- apps/api/src/deliveries/deliveries.module.ts
+++ apps/api/src/deliveries/deliveries.module.ts
@@ -1,9 +1,14 @@
 import { Module } from "@nestjs/common";
 import { DeliveriesController } from "./deliveries.controller";
 import { DeliveriesService } from "./deliveries.service";
+import { DELIVERY_REPOSITORY } from "./deliveries.repository";
+import { DrizzleDeliveryRepository } from "./deliveries.repository.drizzle";
 
 @Module({
   controllers: [DeliveriesController],
-  providers: [DeliveriesService],
+  providers: [
+    DeliveriesService,
+    { provide: DELIVERY_REPOSITORY, useClass: DrizzleDeliveryRepository },
+  ],
 })
 export class DeliveriesModule {}
